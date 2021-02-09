c7884c970445a72f7c63de63c11e4ea8ab84128b
Luis Ruiz <giosterr44@gmail.com>
2021-02-09 17:17:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+var date = "July 3rd";
+
+console.log("My Birthday is " + date);
+
+function happyHolidays(){
+  console.log("Happy holidays!");
+}
+function happyHolidaysTo(){
+  console.log(`Happy holidays, ${name}!`);
+}
+function happyCustomHolidayTo(){
+  console.log(`Happy ${holiday}, ${name}!`);
+}
+function holidayCountdown(){
+  console.log(`It's ${days} days until ${holiday}!`)
+}
